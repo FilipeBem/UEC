b689ea725a6ff043ef1253956cfb6c718fd70d18
FilipeBem <discordso123@gmail.com>
2023-11-28 14:58:40 -0300

    initial commit

---
+++ Combat.java
@@ -0,0 +1,9 @@
+public class Combat{
+    public static void main(String[] args) {
+        
+        Lutador l1 = new Lutador("Prety Boy","Brasil", 21, 1.87f, 75f, 7, 2, 1);
+        
+        l1.apresentar();
+        l1.status();
+    }
+}
